df6d1d7ec357013cb12832733bfdbf50bc2be99a
amoldeshpande05 <amoldeshpande05>
2021-09-19 22:12:58 +0530

    init

--- AutoAI_Backend/server.py
+++ AutoAI_Backend/server.py
@@ -15,7 +15,7 @@ def landing():
 @app.route("/getPrediction")
 def index():
     # NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
-    API_KEY = "6FRDmF9DhIK1Iw7IH9xM9vk8CaRfRhNH_cxKvu1eS3TL"
+    API_KEY = "8tssLsbjkKhkrrqnBJG0C6DzjLTTg7V2rxUA6-PeJgHx"
 
     email = request.args.get("email");
     Gender = request.args.get("Gender");
@@ -35,7 +35,7 @@ def index():
     # payload_scoring = {"input_data": [{"fields": ["Gender","Married","Dependents","Education","Self_Employed","ApplicantIncome","LoanAmount","Loan_Amount_Term","Credit_History","Property_Area"], "values": [[ 1, 1, 1, 1, 0, 4583, 128, 360, 1, "Rural" ]]}]}
     payload_scoring = {"input_data": [{"fields": ["Gender","Married","Dependents","Education","Self_Employed","ApplicantIncome","LoanAmount","Loan_Amount_Term","Credit_History","Property_Area"], "values": [[ Gender, married, dependents, education, Self_Employed, ApplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History, Property_Area ]]}]}
 
-    response_scoring = requests.post('https://eu-gb.ml.cloud.ibm.com/ml/v4/deployments/65e0afca-ba80-4df2-9f69-6b5d086f1c39/predictions?version=2021-09-16', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
+    response_scoring = requests.post('https://eu-gb.ml.cloud.ibm.com/ml/v4/deployments/77902efb-6aed-4a52-800a-54ec2cd1f8c5/predictions?version=2021-09-19', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
     dictionary ={ 
     "status": "200", 
     "response_scoring": response_scoring.json()['predictions'][0]['values'][0][0],
